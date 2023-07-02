a2764b8a068465ddf0058ecbf7bac06404511075
Princeofguilty <Princeofguilty@hotmail.com>
2023-07-03 01:52:46 +0300

    unity print location only

--- src/encoder.cpp
+++ src/encoder.cpp
@@ -636,9 +636,9 @@ int main(int argc, char *argv[])
 		dsrc_broadcast((uint8_t *)&(my_vehicle._location_payload), sizeof(my_vehicle._location_payload));
 		my_vehicle._location_payload.print();
 		usleep(200000);
-		encode_time(my_vehicle._heading_payload._last_time_stamp);
+		// encode_time(my_vehicle._heading_payload._last_time_stamp);
 		dsrc_broadcast((uint8_t *)&(my_vehicle._heading_payload), sizeof(my_vehicle._heading_payload));
-		my_vehicle._heading_payload.print();
+		// my_vehicle._heading_payload.print();
 		// sleep(1);
 		// dsrc_broadcast((uint8_t *)&(my_vehicle._brakes_payload), sizeof(my_vehicle._brakes_payload));
 		// my_vehicle._brakes_payload.print();
